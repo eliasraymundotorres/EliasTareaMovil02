b34e3d526f705dae44016a1004038a117862d888
ShopVentas <eliasrrt@gmail.com>
2021-09-18 22:19:01 -0500

    EliasTare2

---
+++ app/src/main/java/com/example/toolbar002/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.toolbar002;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item1:
+                Toast.makeText(this,"Buscar",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(this,"Carpeta",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item3:
+                return true;
+            default:
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
